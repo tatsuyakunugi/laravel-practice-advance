dc9d08ca83c90043906e4ebfd20a17aa090c9288
tatsuya kunugi <quercus.acutissima.tree@gmail.com>
2025-05-02 16:51:25 +0900

    Todoの作成

---
+++ src/resources/views/layouts/app.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{ asset('css/sanitize.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/common.css') }}">
+    @yield('css')
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <div class="header__inner">
+            <a class="header__logo" href="/">laravel-practice-advance</a>
+        </div>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+</body>
+</html>
