66d05e21a4351cfc0baaf180f0d3b2d0397fefd9
tatsuya kunugi <quercus.acutissima.tree@gmail.com>
2025-05-03 16:45:22 +0900

    Todo、Categoryの作成

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -4,19 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Todo;
+use App\Models\Category;
 use App\Http\Requests\TodoRequest;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        $todos = Todo::all();
-        return view('index', compact('todos'));
+        $todos = Todo::with('category')->get();
+        $categories = Category::all();
+        return view('index', compact('todos', 'categories'));
     }
 
     public function store(TodoRequest $request)
     {
-        $todo = $request->only(['content']);
+        $todo = $request->only([
+            'category_id',
+            'content'
+        ]);
         Todo::create($todo);
 
         return redirect('/')->with('message', 'Todoを作成しました');
@@ -35,5 +40,13 @@ class TodoController extends Controller
         Todo::find($request->id)->delete();
 
         return redirect('/')->with('message', 'Todoを削除しました');
+    }
+
+    public function search(Request $request)
+    {
+        $todos = Todo::with('category')->CategorySearch($request->category_id)->KeywordSearch($request->keyword)->get();
+        $categories = Category::all();
+
+        return view('index', compact('todos', 'categories'));
     }
 }
--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -11,7 +11,14 @@
 <body>
     <header class="header">
         <div class="header__inner">
-            <a class="header__logo" href="/">laravel-practice-advance</a>
+            <div class="header-utilities">
+                <a class="header__logo" href="/">laravel-practice-advance</a>
+                <nav class="header-nav">
+                    <li class="header-nav__item">
+                        <a class="header-nav__link" href="/categories">カテゴリー一覧</a>
+                    </li>
+                </nav>
+            </div>
         </div>
     </header>
     <main>
